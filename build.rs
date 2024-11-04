6f6b0efb251a8a1db4a0486fd7b871af0f23eb6b
Philipp Hoenisch <philipp@coblox.tech>
2024-11-04 15:39:52 +1100

    fix: use tonic to generate types and grpc client

--- build.rs
+++ build.rs
@@ -1,23 +1,20 @@
 /// To generate updated proto objects:
 /// run `RUSTFLAGS="--cfg genproto" cargo build`
 fn main() -> std::io::Result<()> {
-    // #[cfg(genproto)]
+    #[cfg(genproto)]
     generate_protos()?;
     Ok(())
 }
 
-// #[cfg(genproto)]
+#[cfg(genproto)]
 fn generate_protos() -> std::io::Result<()> {
     tonic_build::configure()
         .build_server(false)
         .build_client(true)
         .out_dir("src/generated") // you can change the generated code's location
         .compile_protos(
-            &["src/protos/v1/admin.proto"],
-            &[
-                "src/protos/google/api/http.proto",
-                "src/protos/google/api/annotations.proto",
-            ], // specify the root location to search proto dependencies
+            &["proto/v1/admin.proto"],
+            &["proto"], // specify the root location to search proto dependencies
         )?;
     Ok(())
 }
