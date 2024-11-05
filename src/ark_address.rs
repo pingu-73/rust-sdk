8060fc1962d3ce25fcec273765110f28e0fbf018
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2024-11-05 18:00:03 +1100

    feat: Implement checking offchain balance with ASP
    If correctly implemented, this will allow us to check if a user can
    board the Ark correctly, once we implement boarding.

---
+++ src/ark_address.rs
@@ -0,0 +1,86 @@
+use crate::error::Error;
+use bech32::Bech32m;
+use bech32::Hrp;
+use bitcoin::Network;
+use bitcoin::XOnlyPublicKey;
+
+#[derive(Debug, Clone, Copy)]
+pub struct ArkAddress {
+    hrp: Hrp,
+    asp: XOnlyPublicKey,
+    vtxo_tap_key: XOnlyPublicKey,
+}
+
+impl ArkAddress {
+    pub fn new(network: Network, asp: XOnlyPublicKey, vtxo_tap_key: XOnlyPublicKey) -> Self {
+        let hrp = match network {
+            Network::Bitcoin => "ark",
+            _ => "tark",
+        };
+
+        let hrp = Hrp::parse_unchecked(hrp);
+
+        Self {
+            hrp,
+            asp,
+            vtxo_tap_key,
+        }
+    }
+
+    pub fn decode(value: &str) -> Result<Self, Error> {
+        let (hrp, bytes) = bech32::decode(value).unwrap();
+
+        let asp = XOnlyPublicKey::from_slice(&bytes[..32]).unwrap();
+        let vtxo_tap_key = XOnlyPublicKey::from_slice(&bytes[32..]).unwrap();
+
+        Ok(Self {
+            hrp,
+            asp,
+            vtxo_tap_key,
+        })
+    }
+
+    pub fn encode(&self) -> Result<String, Error> {
+        let mut bytes = [0u8; 64];
+
+        bytes[..32].copy_from_slice(&self.asp.serialize());
+        bytes[32..].copy_from_slice(&self.vtxo_tap_key.serialize());
+
+        let s = bech32::encode::<Bech32m>(self.hrp, bytes.as_slice()).unwrap();
+
+        Ok(s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bitcoin::hex::DisplayHex;
+
+    // Taken from https://github.com/ark-network/ark/blob/b536a9e65252573aaa48110ef5d0c90894eb550c/common/fixtures/encoding.json.
+    #[tokio::test]
+    pub async fn roundtrip() {
+        let address = "tark1x0lm8hhr2wc6n6lyemtyh9rz8rg2ftpkfun46aca56kjg3ws0tsztfpuanaquxc6faedvjk3tax0575y6perapg3e95654pk8r4fjecs5fyd2";
+
+        let decoded = ArkAddress::decode(address).unwrap();
+
+        let hrp = decoded.hrp.to_string();
+        assert_eq!(hrp, "tark");
+
+        let asp = decoded.asp.serialize().as_hex().to_string();
+        assert_eq!(
+            asp,
+            "33ffb3dee353b1a9ebe4ced64b946238d0a4ac364f275d771da6ad2445d07ae0"
+        );
+
+        let vtxo_tap_key = decoded.vtxo_tap_key.serialize().as_hex().to_string();
+        assert_eq!(
+            vtxo_tap_key,
+            "25a43cecfa0e1b1a4f72d64ad15f4cfa7a84d0723e8511c969aa543638ea9967"
+        );
+
+        let encoded = decoded.encode().unwrap();
+
+        assert_eq!(encoded, address);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
+use crate::ark_address::ArkAddress;
 use crate::generated::ark::v1::ark_service_client::ArkServiceClient;
 use crate::generated::ark::v1::GetInfoRequest;
 use crate::generated::ark::v1::GetInfoResponse;
+use crate::generated::ark::v1::ListVtxosRequest;
 use bitcoin::key::Keypair;
 use bitcoin::key::PublicKey;
 use bitcoin::key::Secp256k1;
 use bitcoin::secp256k1::All;
 use bitcoin::Address;
+use bitcoin::Amount;
 use bitcoin::OutPoint;
 use bitcoin::XOnlyPublicKey;
 use error::Error;
@@ -18,6 +21,7 @@ use rand::CryptoRng;
 use rand::Rng;
 use std::collections::HashMap;
 use std::str::FromStr;
+use tonic::transport::Channel;
 
 pub mod generated {
     #[path = ""]
@@ -27,7 +31,11 @@ pub mod generated {
     }
 }
 
+pub mod ark_address;
 pub mod error;
+
+// TODO: Create a module to wrap the `ArkServiceClient`.
+// TODO: Figure out how to integrate on-chain wallet. Probably use a trait and implement using `bdk`.
 
 /// The Miniscript descriptor used for the boarding script.
 ///
@@ -36,28 +44,72 @@ pub mod error;
 /// We use `USER_0` and `USER_1` for the same user key, because `rust-miniscript` does not allow
 /// repeating identifiers.
 const BOARDING_DESCRIPTOR_TEMPLATE_MINISCRIPT: &str =
-    "tr(UNSPENDABLE_KEY,{and_v(v:pk(USER_0),older(604672)),and_v(v:pk(USER_1),pk(ASP))})";
+    "tr(UNSPENDABLE_KEY,{and_v(v:pk(USER_0),older(TIMEOUT)),and_v(v:pk(USER_1),pk(ASP))})";
+
+/// The Miniscript descriptor used for the default VTXO.
+///
+/// We expect the ASP to provide this, but at the moment the ASP does not quite speak Miniscript.
+///
+/// We use `USER_0` and `USER_1` for the same user key, because `rust-miniscript` does not allow
+/// repeating identifiers.
+const DEFAULT_VTXO_DESCRIPTOR_TEMPLATE_MINISCRIPT: &str =
+    "tr(UNSPENDABLE_KEY,{and_v(v:pk(USER_0),older(TIMEOUT)),and_v(v:pk(USER_1),pk(ASP))})";
 
 const UNSPENDABLE_KEY: &str = "0250929b74c1a04954b78b4b6035e97a5e078a5a0f28ec96d547bfee9ace803ac0";
 
+const BOARDING_REFUND_TIMEOUT: u64 = 604672;
+
 pub struct Client {
+    inner: Option<ArkServiceClient<Channel>>,
     name: String,
     kp: Keypair,
+    // TODO: We need to parse the contents of this response instead of using the `prost` type
+    // directly.
     asp_info: Option<GetInfoResponse>,
 }
 
-struct StrPkTranslator {
-    pk_map: HashMap<String, XOnlyPublicKey>,
+#[derive(Debug, Clone)]
+pub struct DefaultVtxoScript {
+    asp: XOnlyPublicKey,
+    owner: XOnlyPublicKey,
+    exit_delay: u64,
+    descriptor: miniscript::descriptor::Tr<XOnlyPublicKey>,
 }
 
-impl Translator<String, XOnlyPublicKey, ()> for StrPkTranslator {
-    fn pk(&mut self, pk: &String) -> Result<XOnlyPublicKey, ()> {
-        self.pk_map.get(pk).copied().ok_or(())
-    }
+impl DefaultVtxoScript {
+    pub fn new(asp: XOnlyPublicKey, owner: XOnlyPublicKey, exit_delay: u64) -> Result<Self, Error> {
+        let vtxo_descriptor =
+            DEFAULT_VTXO_DESCRIPTOR_TEMPLATE_MINISCRIPT.replace("TIMEOUT", &exit_delay.to_string());
+        let descriptor = Descriptor::<String>::from_str(&vtxo_descriptor).unwrap();
 
-    // We don't need to implement these methods as we are not using them in the policy.
-    // Fail if we encounter any hash fragments. See also translate_hash_clone! macro.
-    translate_hash_fail!(String, XOnlyPublicKey, ());
+        debug_assert!(descriptor.sanity_check().is_ok());
+
+        let unspendable_key: PublicKey = UNSPENDABLE_KEY.parse().unwrap();
+        let unspendable_key = unspendable_key.to_x_only_pubkey();
+
+        let mut pk_map = HashMap::new();
+
+        pk_map.insert("UNSPENDABLE_KEY".to_string(), unspendable_key);
+        pk_map.insert("USER_0".to_string(), owner);
+        pk_map.insert("USER_1".to_string(), owner);
+        pk_map.insert("ASP".to_string(), asp);
+
+        let mut t = StrPkTranslator { pk_map };
+
+        let real_desc = descriptor.translate_pk(&mut t).unwrap();
+
+        let tr = match real_desc {
+            Descriptor::Tr(tr) => tr,
+            _ => unreachable!("Descriptor must be taproot"),
+        };
+
+        Ok(Self {
+            asp,
+            owner,
+            exit_delay,
+            descriptor: tr,
+        })
+    }
 }
 
 impl Client {
@@ -68,6 +120,7 @@ impl Client {
         let kp = Keypair::new(&secp, rng);
 
         Self {
+            inner: None,
             name,
             kp,
             asp_info: None,
@@ -83,7 +136,37 @@ impl Client {
 
         self.asp_info = Some(response);
 
+        self.inner = Some(client);
+
         Ok(())
+    }
+
+    // At the moment we are always generating the same address.
+    fn get_offchain_address(&self) -> Result<ArkAddress, Error> {
+        let asp_info = self.asp_info.clone().unwrap();
+
+        let asp: PublicKey = asp_info.pubkey.parse().unwrap();
+        let asp = asp.to_x_only_pubkey();
+        let owner = self.kp.public_key().to_x_only_pubkey();
+
+        let exit_delay = asp_info.unilateral_exit_delay as u64;
+
+        let vtxo_script = DefaultVtxoScript::new(asp, owner, exit_delay).unwrap();
+
+        let vtxo_tap_key = vtxo_script.descriptor.internal_key();
+
+        let network = asp_info.network;
+        let network = network.parse().unwrap();
+
+        let ark_address = ArkAddress::new(network, asp, *vtxo_tap_key);
+
+        Ok(ark_address)
+    }
+
+    fn get_offchain_addresses(&self) -> Result<Vec<ArkAddress>, Error> {
+        let address = self.get_offchain_address().unwrap();
+
+        Ok(vec![address])
     }
 
     fn new_boarding_address(&self) -> Result<Address, Error> {
@@ -96,8 +179,9 @@ impl Client {
         // TODO: Use descriptor from ASP when the ASP supports Miniscript.
         // let boarding_descriptor = asp_info.boarding_descriptor_template.replace(' ', "");
 
-        let boarding_descriptor =
-            Descriptor::<String>::from_str(BOARDING_DESCRIPTOR_TEMPLATE_MINISCRIPT).unwrap();
+        let boarding_descriptor = BOARDING_DESCRIPTOR_TEMPLATE_MINISCRIPT
+            .replace("TIMEOUT", &BOARDING_REFUND_TIMEOUT.to_string());
+        let boarding_descriptor = Descriptor::<String>::from_str(&boarding_descriptor).unwrap();
 
         debug_assert!(boarding_descriptor.sanity_check().is_ok());
 
@@ -125,16 +209,53 @@ impl Client {
         Ok(address)
     }
 
-    fn new_offchain_address(&self) -> Result<Address, Error> {
-        // TODO: implement me
-        let address = Address::from_str("33iFwdLuRpW1uK1RTRqsoi8rR4NpDzk66k").unwrap();
-        Ok(address.assume_checked())
+    async fn offchain_balance(&self) -> Result<Amount, Error> {
+        let addresses: Vec<ArkAddress> = self.get_offchain_addresses()?;
+
+        let mut total = Amount::ZERO;
+        for address in addresses.into_iter() {
+            let address = address.encode()?;
+            let res = self
+                .inner
+                .clone()
+                .unwrap()
+                .list_vtxos(ListVtxosRequest { address })
+                .await
+                .unwrap();
+
+            let sum = res
+                .into_inner()
+                .spendable_vtxos
+                .iter()
+                .fold(Amount::ZERO, |acc, x| acc + Amount::from_sat(x.amount));
+
+            total += sum;
+        }
+
+        Ok(total)
+    }
+
+    fn new_offchain_address(&self) -> Result<ArkAddress, Error> {
+        todo!()
     }
 
     fn board(&self, outpoint: OutPoint) -> Result<(), Error> {
-        // TODO: implement me
-        Ok(())
+        todo!()
     }
+}
+
+struct StrPkTranslator {
+    pk_map: HashMap<String, XOnlyPublicKey>,
+}
+
+impl Translator<String, XOnlyPublicKey, ()> for StrPkTranslator {
+    fn pk(&mut self, pk: &String) -> Result<XOnlyPublicKey, ()> {
+        self.pk_map.get(pk).copied().ok_or(())
+    }
+
+    // We don't need to implement these methods as we are not using them in the policy.
+    // Fail if we encounter any hash fragments. See also translate_hash_clone! macro.
+    translate_hash_fail!(String, XOnlyPublicKey, ());
 }
 
 #[cfg(test)]
@@ -207,7 +328,7 @@ pub mod tests {
     }
 
     #[tokio::test]
-    pub async fn test() {
+    pub async fn e2e() {
         let alice = setup_client("alice".to_string()).await;
 
         // This is just an on-chain address.
@@ -216,5 +337,9 @@ pub mod tests {
         let boarding_output = faucet_fund(alice_boarding_address, Amount::ONE_BTC).await;
 
         println!("Boarding output: {boarding_output:?}");
+
+        let offchain_balance = alice.offchain_balance().await.unwrap();
+
+        println!("Alice offchain balance: {offchain_balance}");
     }
 }
