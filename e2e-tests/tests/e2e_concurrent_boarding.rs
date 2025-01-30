29f74c666ce3bae08e8d15a81d5c5e1253cb916e
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2025-01-30 15:57:21 +1100

    chore(e2e): Add assertions to e2e_concurrent_boarding test

--- e2e-tests/tests/e2e_concurrent_boarding.rs
+++ e2e-tests/tests/e2e_concurrent_boarding.rs
@@ -1,19 +1,14 @@
 #![allow(clippy::unwrap_used)]
 
-use ark_bdk_wallet::Wallet;
-use ark_core::BoardingOutput;
-use ark_rs::wallet::BoardingWallet;
-use ark_rs::Client;
 use bitcoin::key::Secp256k1;
 use bitcoin::Amount;
 use common::init_tracing;
 use common::set_up_client;
-use common::InMemoryDb;
 use common::Nigiri;
 use rand::rngs::StdRng;
 use rand::SeedableRng;
-use std::borrow::Borrow;
 use std::sync::Arc;
+use std::time::Duration;
 use tokio::try_join;
 
 mod common;
@@ -26,48 +21,50 @@ pub async fn concurrent_boarding() {
 
     let secp = Secp256k1::new();
 
-    let (alice, alice_wallet) =
-        set_up_client("alice".to_string(), nigiri.clone(), secp.clone()).await;
+    let (alice, _) = set_up_client("alice".to_string(), nigiri.clone(), secp.clone()).await;
+    let (bob, _) = set_up_client("bob".to_string(), nigiri.clone(), secp.clone()).await;
+    let (claire, _) = set_up_client("claire".to_string(), nigiri.clone(), secp.clone()).await;
 
-    let (bob, bob_wallet) = set_up_client("bob".to_string(), nigiri.clone(), secp.clone()).await;
-
-    let (claire, claire_wallet) =
-        set_up_client("claire".to_string(), nigiri.clone(), secp.clone()).await;
-
-    let alice_boarding_output = new_boarding_output(&alice, alice_wallet).await;
-    let bob_boarding_output = new_boarding_output(&bob, bob_wallet).await;
-    let claire_boarding_output = new_boarding_output(&claire, claire_wallet).await;
-
-    let alice_initial_balance = Amount::ONE_BTC;
-    let alice_boarding_output = nigiri
-        .faucet_fund(alice_boarding_output.address(), alice_initial_balance)
-        .await;
-
-    let bob_initial_balance = Amount::ONE_BTC;
-    let bob_boarding_output = nigiri
-        .faucet_fund(bob_boarding_output.address(), bob_initial_balance)
-        .await;
-
-    let claire_initial_balance = Amount::ONE_BTC;
-    let claire_boarding_output = nigiri
-        .faucet_fund(claire_boarding_output.address(), claire_initial_balance)
-        .await;
-
-    tracing::debug!("Boarding output alice: {alice_boarding_output:?}");
-    tracing::debug!("Boarding output bob: {bob_boarding_output:?}");
-    tracing::debug!("Boarding output claire: {claire_boarding_output:?}");
+    let alice_boarding_output = alice.get_boarding_output().unwrap();
+    let bob_boarding_output = bob.get_boarding_output().unwrap();
+    let claire_boarding_output = claire.get_boarding_output().unwrap();
 
     let alice_offchain_balance = alice.offchain_balance().await.unwrap();
     let bob_offchain_balance = bob.offchain_balance().await.unwrap();
     let claire_offchain_balance = claire.offchain_balance().await.unwrap();
-    tracing::debug!("Pre boarding: Alice offchain balance: {alice_offchain_balance:?}");
-    tracing::debug!("Pre boarding: Bob offchain balance: {bob_offchain_balance:?}");
-    tracing::debug!("Pre boarding: Claire offchain balance: {claire_offchain_balance:?}");
 
-    let alice_task = tokio::spawn(async move {
-        let mut rng = StdRng::from_entropy();
-        alice.board(&mut rng).await.unwrap();
-        alice
+    assert_eq!(alice_offchain_balance.total(), Amount::ZERO);
+    assert_eq!(bob_offchain_balance.total(), Amount::ZERO);
+    assert_eq!(claire_offchain_balance.total(), Amount::ZERO);
+
+    let alice_fund_amount = Amount::from_sat(200_000_000);
+    let bob_fund_amount = Amount::ONE_BTC;
+    let claire_fund_amount = Amount::from_sat(50_000_000);
+
+    nigiri
+        .faucet_fund(alice_boarding_output.address(), alice_fund_amount)
+        .await;
+    nigiri
+        .faucet_fund(bob_boarding_output.address(), bob_fund_amount)
+        .await;
+    nigiri
+        .faucet_fund(claire_boarding_output.address(), claire_fund_amount)
+        .await;
+
+    let alice_offchain_balance = alice.offchain_balance().await.unwrap();
+    let bob_offchain_balance = bob.offchain_balance().await.unwrap();
+    let claire_offchain_balance = claire.offchain_balance().await.unwrap();
+
+    assert_eq!(alice_offchain_balance.total(), Amount::ZERO);
+    assert_eq!(bob_offchain_balance.total(), Amount::ZERO);
+    assert_eq!(claire_offchain_balance.total(), Amount::ZERO);
+
+    let alice_task = tokio::spawn({
+        async move {
+            let mut rng = StdRng::from_entropy();
+            alice.board(&mut rng).await.unwrap();
+            alice
+        }
     });
 
     let bob_task = tokio::spawn(async move {
@@ -82,64 +79,112 @@ pub async fn concurrent_boarding() {
         claire
     });
 
+    // Three parties joining a round concurrently.
     let (alice, bob, claire) = try_join!(alice_task, bob_task, claire_task).unwrap();
+    tokio::time::sleep(Duration::from_secs(2)).await;
 
     let alice_offchain_balance = alice.offchain_balance().await.unwrap();
     let bob_offchain_balance = bob.offchain_balance().await.unwrap();
     let claire_offchain_balance = claire.offchain_balance().await.unwrap();
-    tracing::debug!("Post boarding: Alice offchain balance: {alice_offchain_balance:?}");
-    tracing::debug!("Post boarding: Bob offchain balance: {bob_offchain_balance:?}");
-    tracing::debug!("Post boarding: Claire offchain balance: {claire_offchain_balance:?}");
 
+    assert_eq!(alice_offchain_balance.confirmed(), alice_fund_amount);
+    assert_eq!(alice_offchain_balance.pending(), Amount::ZERO);
+    assert_eq!(bob_offchain_balance.confirmed(), bob_fund_amount);
+    assert_eq!(bob_offchain_balance.pending(), Amount::ZERO);
+    assert_eq!(claire_offchain_balance.confirmed(), claire_fund_amount);
+    assert_eq!(claire_offchain_balance.pending(), Amount::ZERO);
+
+    let (alice_offchain_address, _) = alice.get_offchain_address();
     let (bob_offchain_address, _) = bob.get_offchain_address();
-    let amount = Amount::from_sat(100_000);
+    let (claire_offchain_address, _) = claire.get_offchain_address();
 
-    bob.list_vtxos().await.unwrap();
+    let alice_to_bob_send_amount = Amount::from_sat(100_000);
+    let bob_to_claire_send_amount = Amount::from_sat(50_000);
+    let claire_to_alice_send_amount = Amount::from_sat(10_000);
 
-    let alice_task = tokio::spawn(async move {
-        tracing::debug!("Alice is sending {amount} to Bob offchain...");
-        alice.send_vtxo(bob_offchain_address, amount).await.unwrap();
-        alice
-    });
-    let claire_task = tokio::spawn(async move {
-        tracing::debug!("Claire is sending {amount} to Bob offchain...");
-        claire
-            .send_vtxo(bob_offchain_address, amount)
-            .await
-            .unwrap();
-        claire
-    });
+    let alice_to_bob_redeem_tx = alice
+        .send_vtxo(bob_offchain_address, alice_to_bob_send_amount)
+        .await
+        .unwrap();
+    let alice_to_bob_redeem_tx_fee = alice_to_bob_redeem_tx.fee().unwrap();
+    let bob_to_claire_redeem_tx = bob
+        .send_vtxo(claire_offchain_address, bob_to_claire_send_amount)
+        .await
+        .unwrap();
+    let bob_to_claire_redeem_tx_fee = bob_to_claire_redeem_tx.fee().unwrap();
+    let claire_to_alice_redeem_tx = claire
+        .send_vtxo(alice_offchain_address, claire_to_alice_send_amount)
+        .await
+        .unwrap();
+    let claire_to_alice_redeem_tx_fee = claire_to_alice_redeem_tx.fee().unwrap();
 
-    let (_alice, _claire) = try_join!(alice_task, claire_task).unwrap();
-
+    let alice_offchain_balance = alice.offchain_balance().await.unwrap();
     let bob_offchain_balance = bob.offchain_balance().await.unwrap();
-    let bob_vtxos = bob.list_vtxos().await.unwrap();
-    tracing::debug!(
-        ?bob_vtxos,
-        "Post payment: Bob offchain balance: {bob_offchain_balance:?}"
+    let claire_offchain_balance = claire.offchain_balance().await.unwrap();
+
+    assert_eq!(alice_offchain_balance.confirmed(), Amount::ZERO);
+    assert_eq!(
+        alice_offchain_balance.pending(),
+        alice_fund_amount - alice_to_bob_send_amount - alice_to_bob_redeem_tx_fee
+            + claire_to_alice_send_amount
     );
+    assert_eq!(bob_offchain_balance.confirmed(), Amount::ZERO);
+    assert_eq!(
+        bob_offchain_balance.pending(),
+        bob_fund_amount - bob_to_claire_send_amount - bob_to_claire_redeem_tx_fee
+            + alice_to_bob_send_amount
+    );
+    assert_eq!(claire_offchain_balance.confirmed(), Amount::ZERO);
+    assert_eq!(
+        claire_offchain_balance.pending(),
+        claire_fund_amount - claire_to_alice_send_amount - claire_to_alice_redeem_tx_fee
+            + bob_to_claire_send_amount
+    );
+
+    let alice_task = tokio::spawn({
+        async move {
+            let mut rng = StdRng::from_entropy();
+            alice.board(&mut rng).await.unwrap();
+            alice
+        }
+    });
+
+    let bob_task = tokio::spawn(async move {
+        let mut rng = StdRng::from_entropy();
+        bob.board(&mut rng).await.unwrap();
+        bob
+    });
+
+    let claire_task = tokio::spawn(async move {
+        let mut rng = StdRng::from_entropy();
+        claire.board(&mut rng).await.unwrap();
+        claire
+    });
+
+    // Three parties joining a round concurrently.
+    let (alice, bob, claire) = try_join!(alice_task, bob_task, claire_task).unwrap();
+    tokio::time::sleep(Duration::from_secs(2)).await;
+
+    let alice_offchain_balance = alice.offchain_balance().await.unwrap();
+    let bob_offchain_balance = bob.offchain_balance().await.unwrap();
+    let claire_offchain_balance = claire.offchain_balance().await.unwrap();
 
     assert_eq!(
-        bob_offchain_balance.total(),
-        bob_initial_balance + amount * 2
+        alice_offchain_balance.confirmed(),
+        alice_fund_amount - alice_to_bob_send_amount - alice_to_bob_redeem_tx_fee
+            + claire_to_alice_send_amount
     );
-}
-
-async fn new_boarding_output(
-    client: &Client<Nigiri, Wallet<InMemoryDb>>,
-    wallet: impl Borrow<Wallet<InMemoryDb>>,
-) -> BoardingOutput {
-    let asp_info = client.asp_info.clone();
-    let asp_pk = asp_info.pk;
-    let (asp_pk, _) = asp_pk.inner.x_only_public_key();
-
-    wallet
-        .borrow()
-        .new_boarding_output(
-            asp_pk,
-            asp_info.round_lifetime,
-            &asp_info.boarding_descriptor_template,
-            asp_info.network,
-        )
-        .unwrap()
+    assert_eq!(alice_offchain_balance.pending(), Amount::ZERO);
+    assert_eq!(
+        bob_offchain_balance.confirmed(),
+        bob_fund_amount - bob_to_claire_send_amount - bob_to_claire_redeem_tx_fee
+            + alice_to_bob_send_amount
+    );
+    assert_eq!(bob_offchain_balance.pending(), Amount::ZERO);
+    assert_eq!(
+        claire_offchain_balance.confirmed(),
+        claire_fund_amount - claire_to_alice_send_amount - claire_to_alice_redeem_tx_fee
+            + bob_to_claire_send_amount
+    );
+    assert_eq!(claire_offchain_balance.pending(), Amount::ZERO);
 }
